2edeb5fe05cee974162b28858de58209b03f44cd
Shineast <canfrog@naver.com>
2025-10-27 19:00:16 +0900

    코드 순서대로 테스트를 만들자

---
+++ jest_practical/controllers/auth.spec.js
@@ -0,0 +1,23 @@
+const { join } = require("./auth.js");
+
+describe("join", () => {
+  test("Email이 없으면 Email이 없다면 Frontend로 no_email 에러를 QueryString으로 보낸다.", async () => {
+    const req = {
+      body: {
+        email: "",
+        password: "password",
+        nick: "동붕이",
+      },
+    };
+    const res = {
+      redirect: jest.fn(),
+    };
+    const next = () => {};
+    await join(req, res, next);
+    expect(res.redirect).toHaveBeenCalledWith("/join?error=no_email");
+  });
+
+  test("회원가입 도중에 에러가 발생하면 에러를 응답한다.", () => {});
+
+  test("이미 가입한 이메일이 아니면 회원가입을 진행한다. (암호화 후 DB 저장)", () => {});
+});

942c14902b2a80eb77a0323d4f3c38c67318267d
Shineast <canfrog@naver.com>
2025-10-27 21:05:13 +0900

    미래를 대비하는 테스트는 무엇인가

--- jest_practical/controllers/auth.spec.js
+++ jest_practical/controllers/auth.spec.js
@@ -1,4 +1,5 @@
 const { join } = require("./auth.js");
+const User = require("../models/user.js");
 
 describe("join", () => {
   test("Email이 없으면 Email이 없다면 Frontend로 no_email 에러를 QueryString으로 보낸다.", async () => {
@@ -47,6 +48,26 @@ describe("join", () => {
     const next = () => {};
     await join(req, res, next);
     expect(res.redirect).toHaveBeenCalledWith("/join?error=no_password");
+  });
+
+  test("이미 가입한 이메일이라면 에러를 응답한다.", async () => {
+    const req = {
+      body: {
+        email: "dongyoon@email.com",
+        password: "password",
+        nick: "동붕이",
+      },
+    };
+    const res = {
+      redirect: jest.fn(),
+    };
+    const next = () => {};
+    jest.spyOn(User, "findOne").mockResolvedValue({ id: 1 });
+    jest.spyOn(User, "create").mockImplementation();
+
+    await join(req, res, next);
+    expect(res.redirect).toHaveBeenCalledWith("/join?error=exist");
+    expect(User.create).not.toHaveBeenCalled();
   });
 
   test("회원가입 도중에 에러가 발생하면 에러를 응답한다.", () => {});

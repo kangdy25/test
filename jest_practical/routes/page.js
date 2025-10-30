653ed0d37ece2471ff91073f9b4875443a4e10f4
Kangdy <canfrog@naver.com>
2025-10-30 17:14:57 +0900

    커버리지 100%를 향한 여정 (3)

--- jest_practical/routes/page.js
+++ jest_practical/routes/page.js
@@ -1,25 +1,32 @@
-const express = require('express');
-const { isLoggedIn, isNotLoggedIn } = require('../middlewares');
+const express = require("express");
+const { isLoggedIn, isNotLoggedIn } = require("../middlewares");
 const {
-  renderProfile, renderJoin, renderMain, renderHashtag,
-} = require('../controllers/page');
+    renderProfile,
+    renderJoin,
+    renderMain,
+    renderHashtag,
+} = require("../controllers/page");
 
 const router = express.Router();
 
-router.use((req, res, next) => {
-  res.locals.user = req.user;
-  res.locals.followerCount = req.user?.Followers?.length || 0;
-  res.locals.followingCount = req.user?.Followings?.length || 0;
-  res.locals.followingIdList = req.user?.Followings?.map(f => f.id) || [];
-  next();
-});
+const countFollow = (req, res, next) => {
+    res.locals.user = req.user;
+    res.locals.followerCount = req.user?.Followers?.length || 0;
+    res.locals.followingCount = req.user?.Followings?.length || 0;
+    res.locals.followingIdList = req.user?.Followings?.map((f) => f.id) || [];
+    next();
+};
 
-router.get('/profile', isLoggedIn, renderProfile);
+router.countFollow = countFollow;
 
-router.get('/join', isNotLoggedIn, renderJoin);
+router.use(countFollow);
 
-router.get('/', renderMain);
+router.get("/profile", isLoggedIn, renderProfile);
 
-router.get('/hashtag', renderHashtag);
+router.get("/join", isNotLoggedIn, renderJoin);
+
+router.get("/", renderMain);
+
+router.get("/hashtag", renderHashtag);
 
 module.exports = router;
